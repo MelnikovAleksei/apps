5eaa58b20efb2c28c63334cb0d6048656926399f
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-02-29 10:45:36 +0300

    main js

--- stop-watch/main.js
+++ stop-watch/main.js
@@ -2,7 +2,9 @@ let display = document.querySelector('.display');
 let startBtn = document.querySelector('.startBtn');
 let stopBtn = document.querySelector('.stopBtn');
 let resetBtn = document.querySelector('.resetBtn');
-
+let saveBtn = document.querySelector('.saveBtn');
+let deleteSavedBtn = document.querySelector('.deleteSavedBtn');
+let saveCount = '';
 let secondCount = 0;
 
 let stopWatch;
@@ -17,9 +19,9 @@ function displayCount() {
     let displayHours = (hours < 10) ? '0' + hours : hours;
     let displayMinutes = (minutes < 10) ? '0' + minutes : minutes;
     let displaySeconds = (seconds < 10) ? '0' + seconds : seconds;
-
     display.textContent = displayHours + ':' + displayMinutes + ':' + displaySeconds;
     secondCount++;
+    saveCount = displayHours + ':' + displayMinutes + ':' + displaySeconds;
 }
 
 startBtn.addEventListener('click', () => {
@@ -39,7 +41,23 @@ resetBtn.addEventListener('click', () => {
     startBtn.disabled = false;
     secondCount = 0;
     displayCount();
-    startBtn.focus();
 })
 
+saveBtn.addEventListener('click', () => {
+    const div = document.querySelector('div');
+    const para = document.createElement('p');
+    //const deleteBtn = document.createElement('button');
+    
+    div.appendChild(para);
+    //div.appendChild(deleteBtn);
+    para.setAttribute('class', 'savedCount')
+    //deleteBtn.setAttribute('class', 'deleteBtn')
+
+    para.textContent = saveCount;
+    //deleteBtn.textContent = 'Delete'
+})
+
+/*deleteBtn.addEventListener('click', () => {
+
+})*/
 displayCount()
